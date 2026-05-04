2180a8c9b77b32d83186214282b982e9160566db
mk-2006 <rammkr2006@gmail.com>
2026-05-04 12:03:06 +0530

    Add files via upload

---
+++ sarkari_data.js
@@ -0,0 +1,38 @@
+const sarkariData = {
+    topOnlineForms: [
+        { title: "Bihar Police BPSSC Havaldar Instructor Online Form 2026 – Start", link: "https://www.sarkariexam.com/bpssc-havaldar-instructor-2026/" },
+        { title: "UP Polytechnic JEECUP Online Form 2026 – Extend", link: "https://www.sarkariexam.com/up-polytechnic-jeecup-2026/" },
+        { title: "CRPF Constable Tradesmen Online Form 2026 – Start", link: "https://www.sarkariexam.com/crpf-constable-technical-tradesmen-2026/" },
+        { title: "SSC Phase XIV Online Form 2026 (3003 Posts)", link: "https://www.sarkariexam.com/ssc-phase-xiv-2026/" },
+        { title: "RRB ALP CEN 01/2026 Online Form 2026 (11,127 Posts)", link: "https://www.sarkariexam.com/rrb-alp-cen-01-2026/" },
+        { title: "Rajasthan RSSB Teaching Associate Online Form 2026", link: "https://www.sarkariexam.com/rajasthan-rssb-teaching-associate-2026/" },
+        { title: "MPESB Nursing Officer, Sister Tutor Online Form 2026 – Re-Open", link: "https://www.sarkariexam.com/mpesb-nursing-officer-sister-tutor-2026/" },
+        { title: "MPESB Hospital Assistant Online Form 2026", link: "https://www.sarkariexam.com/mpesb-hospital-assistant-2026/" },
+        { title: "Union Bank of India Apprentices Online Form 2026", link: "https://www.sarkariexam.com/union-bank-of-india-apprentices-2026/" },
+        { title: "RBI Grade-B Officer Online Form 2026 – Start", link: "https://www.sarkariexam.com/rbi-grade-b-2026/" }
+    ],
+    admitCards: [
+        { title: "SSC GD Constable Admit Card 2026", link: "https://www.sarkariexam.com/ssc-gd-constable-2025/" },
+        { title: "RRB NTPC 10+2 UG CBT-I Exam City Details 2026", link: "https://www.sarkariexam.com/rrb-ntpc-102-inter-level-2025/" },
+        { title: "MPESB PAT Admit Card 2026", link: "https://www.sarkariexam.com/mpesb-mp-pat-2026/" },
+        { title: "Indian Navy Agniveer SSR/ MR Admit Card 2026", link: "https://www.sarkariexam.com/indian-navy-agniveer-ssr-mr-2026/" },
+        { title: "Indian Navy Agniveer SSR Medical Assistant Admit Card 2026", link: "https://www.sarkariexam.com/indian-navy-agniveer-ssr-medical-assistant-2026/" },
+        { title: "Bihar BSSC Office Attendant Mains Exam Date 2026", link: "https://www.sarkariexam.com/bihar-bssc-office-attendant-2022/" },
+        { title: "BPSSC Bihar Police SI Mains Exam Date 2026", link: "https://www.sarkariexam.com/bpssc-bihar-police-si-2025/" },
+        { title: "Punjab and Sind Bank LBO Exam Date 2026", link: "https://www.sarkariexam.com/punjab-and-sind-bank-lbo-2026/" },
+        { title: "NTA CUET UG Exam City Details 2026", link: "https://www.sarkariexam.com/nta-cuet-ug-2026/" },
+        { title: "NTA NEET UG Admit Card 2026", link: "https://www.sarkariexam.com/nta-neet-ug-2026/" }
+    ],
+    results: [
+        { title: "Maharashtra Board HSC Class 12th Result 2026 – Out", link: "https://www.sarkariexam.com/maharashtra-board-hsc-class-12th-result-2026/" },
+        { title: "CBSE Board 12th Result 2026 – Soon", link: "https://www.sarkariexam.com/cbse-board-class-12th-result-2026/" },
+        { title: "OICL AO Scale-I Mains Result 2026", link: "https://www.sarkariexam.com/oicl-ao-scale-i-2025/" },
+        { title: "UPPSC APO Pre Result 2026", link: "https://www.sarkariexam.com/uppsc-apo-2025/" },
+        { title: "MPESB MP Police Constable Unit Allotment 2026", link: "https://www.sarkariexam.com/mpesb-mp-police-constable-2026/" },
+        { title: "UPSSSC Mandi Parishad Sachiv Grade II Final Result 2026", link: "https://www.sarkariexam.com/upsssc-mandi-parishad-sachiv-grade-ii-2024/" },
+        { title: "Rajasthan RPSC Junior Chemist Result 2026", link: "https://www.sarkariexam.com/rpsc-junior-chemist-2026/" },
+        { title: "CISCE ISC, ICSE 10th, 12th Result 2026 – Declared", link: "https://www.sarkariexam.com/cisce-isc-icse-result-2026/" },
+        { title: "SSC Delhi Police Driver Result 2026", link: "https://www.sarkariexam.com/ssc-delhi-police-driver-2026/" },
+        { title: "Chhattisgarh Board Class 10th & 12th Result 2026 – Declared", link: "https://www.sarkariexam.com/chhattisgarh-board-result-2026/" }
+    ]
+};
